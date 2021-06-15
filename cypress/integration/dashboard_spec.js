bfb8ad13f350021d7c388ea34b59462a39af5656
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-15 13:26:13 -0400

    Test form initial state

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -9,12 +9,18 @@ describe('Dashboard', () => {
 
     it('Should show Url Card', () => {
         cy.load()
-        cy.get('header').should('be.visible')
-          .get('h1').should('be.visible')
           .get('.url').should('be.visible')
           .get('.url > h3').contains('CYPRESS TESTING')
           .get('.url > a').contains('http://localhost:3001/useshorturl/1')
           .get('.url > p').contains('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+    })
+
+    it('Should show form', () => {
+        cy.load()
+          .get('form').should('be.visible')
+          .get('.titleInput').should('be.visible').should('contain', '')
+          .get('.urlInput').should('be.visible').should('contain', '')
+          .get('form > button').should('be.visible').should('contain', 'Shorten Please!')
     })
     
 });
