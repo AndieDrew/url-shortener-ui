dff1e405430bcd408a03a3e4dd9e16c390af2cb8
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-15 12:13:38 -0400

    Fix POST bug

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,12 +5,13 @@ export const getUrls = () => {
 
 
 export const postUrl = ( newUrl ) => {
+  console.log("POST FIRED", newUrl);
   fetch('http://localhost:3001/api/v1/urls', {
       method: 'POST',
       body: JSON.stringify({
         "id": newUrl.id,
         "title": newUrl.title,
-        "urlToShorten": newUrl.urlToShorten,
+        "long_url": newUrl.urlToShorten,
       }),
       headers: {
         'Content-Type': 'application/json'
