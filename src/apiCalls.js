ecde857da50d7fc0f8f14541187f57a68cfe6155
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-15 12:03:55 -0400

    Build fetch POST method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,19 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+
+export const postUrl = ( newUrl ) => {
+  fetch('http://localhost:3001/api/v1/urls', {
+      method: 'POST',
+      body: JSON.stringify({
+        "id": newUrl.id,
+        "title": newUrl.title,
+        "urlToShorten": newUrl.urlToShorten,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+}
