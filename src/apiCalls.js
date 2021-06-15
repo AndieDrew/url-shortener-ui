eb59f7f966e698f3e30025a02d26d08be408325c
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-15 12:51:52 -0400

    Return fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,7 +5,7 @@ export const getUrls = () => {
 
 
 export const postUrl = ( newUrl ) => {
-  fetch('http://localhost:3001/api/v1/urls', {
+  return fetch('http://localhost:3001/api/v1/urls', {
       method: 'POST',
       body: JSON.stringify({
         "id": newUrl.id,
