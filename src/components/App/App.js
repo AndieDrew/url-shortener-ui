d83504c2e219d75867dd5e2bc495fd36a1384b91
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-15 11:52:30 -0400

    Pass addUrl method to form as a prop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -29,7 +29,7 @@ export class App extends Component {
       <main className="App">
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm />
+          <UrlForm addUrl={this.addUrl}/>
         </header>
 
         <UrlContainer urls={this.state.urls}/>
