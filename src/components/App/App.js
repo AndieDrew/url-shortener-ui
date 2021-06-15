6b65f05d266121dda811a29952a889504174efb3
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-15 11:24:13 -0400

    Fetch api on componentDidMount

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,6 +13,11 @@ export class App extends Component {
   }
 
   componentDidMount() {
+    getUrls()
+      .then(data => {
+        this.setState({urls: [...this.state.urls, data]})
+      })
+
   }
 
   render() {
